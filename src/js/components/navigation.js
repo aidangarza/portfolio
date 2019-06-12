932f9d0f2d6dff0bb2a7314fff418e80c2d89b24
aidangarza <aidangarza@gmail.com>
2019-06-12 13:50:11 -0500

    Edited header content for mobile

--- src/js/components/navigation.js
+++ src/js/components/navigation.js
@@ -1,4 +1,5 @@
 (function () {
+  const threshold = (document.body.clientWidth < 829) ? [0.25] : [0.8]
   const input = document.getElementById('anchor')
   const anchors = ['about', 'recommendations', 'experience', 'skills']
 
@@ -10,7 +11,7 @@
 
   for (let anchor of anchors) {
     const node = document.getElementById(anchor)
-    const io = new IntersectionObserver(([entry]) => handleIOChange(entry, anchor), { threshold: [0.8] })
+    const io = new IntersectionObserver(([entry]) => handleIOChange(entry, anchor), { threshold })
 
     io.observe(node)
   }
