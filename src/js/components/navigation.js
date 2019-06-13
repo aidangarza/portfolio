386ae3fe3bb3ca48b9b66d51b5b616bd8870c855
aidangarza <aidangarza@gmail.com>
2019-06-13 11:15:54 -0500

    Added gate to navigation.js so script doesn't break on browsers that do not support IO

--- src/js/components/navigation.js
+++ src/js/components/navigation.js
@@ -1,18 +1,21 @@
 (function () {
-  const threshold = (document.body.clientWidth < 829) ? [0.25] : [0.8]
-  const input = document.getElementById('anchor')
-  const anchors = ['about', 'recommendations', 'experience', 'skills']
+  if ('IntersectionObserver' in window) {
+    const threshold = (document.body.clientWidth < 829) ? [0.25] : [0.8]
+    const input = document.getElementById('anchor')
+    const anchors = ['about', 'recommendations', 'experience', 'skills']
 
-  function handleIOChange (entry, anchor) {
-    if (entry.isIntersecting) {
-      input.value = (anchor === 'about') ? ' M. Garza, Sr.' : `'s ${anchor}`
+    function handleIOChange (entry, anchor) {
+      if (entry.isIntersecting) {
+        // Set the value in the header input
+        input.value = (anchor === 'about') ? ' M. Garza, Sr.' : `'s ${anchor}`
+      }
     }
-  }
 
-  for (let anchor of anchors) {
-    const node = document.getElementById(anchor)
-    const io = new IntersectionObserver(([entry]) => handleIOChange(entry, anchor), { threshold })
+    for (let anchor of anchors) {
+      const node = document.getElementById(anchor)
+      const io = new IntersectionObserver(([entry]) => handleIOChange(entry, anchor), { threshold })
 
-    io.observe(node)
+      io.observe(node)
+    }
   }
 })()
